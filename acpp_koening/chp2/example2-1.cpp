10e2c9d4729f35db423ace1bf10d035f4ff7825f
Moses Huang <hhuang05@gmail.com>
2019-05-16 16:01:21 -0400

    Updated implementation

--- acpp_koening/chp2/example2-1.cpp
+++ acpp_koening/chp2/example2-1.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <string>
 
+std::string add_border(std::string input)
+{
+  return "*" + input + "*";
+}
+
 int main()
 {
   // Asks for the person's name
@@ -13,50 +18,30 @@ int main()
   // Build message that we intend to write
   const std::string greeting = "Hello, " + name + "!";
 
-  // Have to rewrite
-  // Number of blanks surrounding greeting
-  const int rowpad = 1;
-  const int colpad = 5;
-  const int rows = rowpad * 2 + 3;
-
-  // separate output from input
-  std::cout << std::endl;
-
-  // Type to use for size of string
-  const std::string::size_type cols = greeting.size() + colpad * 2 + 2;
+  // Accounting for padding
+  const unsigned int rowpad = 2; // no of padding rows
+  const unsigned int colpad = 3; // no of col spacers
   
-  // write rows of output
-  for (int r = 0; r != rows; ++r) {
+  // Number of rows: 2 for frame, 1 for message, then have to factor in padding
+  const unsigned int rows = rowpad * 2 + 3; 
 
-    std::string::size_type c = 0;
-    
-    // invariant: we have written c characters so far in the current row    
-    while (c != cols) {
-      
-      // If we are writing the message
-      if (r == rowpad+1 && c == colpad+1) {
-        std::cout << greeting;
-        c += greeting.size();
-      } else {
-        // If we are on the border
-        if (r == 0 || r == rows-1 || c == 0 || c == cols-1) {
-          std::cout << "*"; //Yes, write *
-          ++c; 
-        } else {         // If we are NOT on the border
-          if (r != rowpad+1) {
-            std::string spaces(cols-2, ' ');
-            std::cout << spaces;
-            c += spaces.size();;
-          } else { //Now we're on the message row
-            std::string spaces(colpad, ' ');
-            std::cout << spaces;
-            c += spaces.size();;
-          }
-        }
-      }      
+  const std::string colpad_str = std::string(colpad, ' ');
+  const std::string final_greeting = colpad_str + greeting + colpad_str;
+  const std::string rowpad_str = std::string(final_greeting.size(), ' ');
+  const std::string frame = std::string(final_greeting.size(), '*');
+
+  for (unsigned int r = 0; r < rows; ++r) {
+    if (r == 0) {
+      std::cout << add_border(frame) << std::endl;
+    } else if (r <= rowpad) {
+      std::cout << add_border(rowpad_str) << std::endl;
+    } else if (r == rowpad+1) {
+      std::cout << add_border(final_greeting) << std::endl;
+    } else if (r < rows-1) {
+      std::cout << add_border(rowpad_str) << std::endl;
+    } else {
+      std::cout << add_border(frame) << std::endl;
     }
-
-    std::cout << std::endl;
   }
   
   return 0;

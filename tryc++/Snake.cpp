3dcb0a0450862349cb105fcfc51e51cf58cd704f
Moses Huang <mhuang@ah-mhuang-maci.dhcp.mathworks.com>
2019-08-14 11:04:19 -0400

    Border finished

--- tryc++/Snake.cpp
+++ tryc++/Snake.cpp
@@ -11,17 +11,28 @@ void Snake::snakeSetUp()
 {
 }
 
+void Snake::drawGameBoard()
+{
+
+}
+
 void Snake::drawBorder()
 {
-  std::string hor_line(Snake::border_width, '*');
+  std::string hor_line(Snake::border_width - 2, '*');
 
   for (int i = 0; i < Snake::border_height; ++i) {
+    // Draw left border
+    placeXY(0, i);
+    std::cout << "*";
     if (i == 0 or i == (Snake::border_height - 1)) {
-      std::cout << hor_line << "\n";
-    } else {
-      
-    }
+      std::cout << hor_line;
+    } 
+
+    // Draw right border
+    placeXY(Snake::border_width - 1, i);
+    std::cout << "*";
   }
+  std::cout << std::endl;
 }
 
 void Snake::draw()
--- tryc++/Snake.hpp
+++ tryc++/Snake.hpp
@@ -14,6 +14,7 @@ private:
   std::pair<int, int> m_headPosition;
   std::pair<int, int> m_fruitPosition;
 
+  void drawGameBoard();
   void drawBorder();
 
 public:
